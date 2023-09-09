18551888dc5970b41b9ddd47cc9e92ee2cbfd2bb
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-09-09 20:43:22 +0530

    BlogBackEndDone

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,5 +1,15 @@
 const blogModel = require("../models/blogModel");
 
+
+async function readBlog(req, res) {
+    try {
+        const result = await blogModel.find()
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
 async function createBlog(req, res) {
     const data = req.body;
     console.log(data);
@@ -11,7 +21,41 @@ async function createBlog(req, res) {
     }
 }
 
+async function updateBlog(req, res) {
+    const {id} = req.params
+    const data = req.body;
+    try {
+        const isExist = await blogModel.findById(id)
+        if(isExist){
+            const result = await blogModel.findByIdAndUpdate(id,data);
+            res.status(200).json(result)
+        }else{
+            res.status(500).json({error: 'Blog Not Exist'})
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+async function deleteBlog(req, res) {
+    const {id} = req.params
+    try {
+        const isExist = await blogModel.findById(id)
+        
+        if(isExist){
+            const result = await blogModel.findByIdAndDelete(id);
+            res.status(200).json({message: 'Successfully Deleted', result: result})
+        }else{
+            res.status(500).json({error: 'Blog Not Exist'})
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
 
 module.exports = {
-    createBlog: createBlog
+    createBlog: createBlog,
+    updateBlog: updateBlog,
+    deleteBlog : deleteBlog,
+    readBlog: readBlog
 };
