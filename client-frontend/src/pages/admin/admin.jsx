a816343fb1ebfc40f46a86262948e66c62e5fd0d
IT21158704 <it21158704@my.sliit.lk>
2023-09-09 19:13:08 +0530

    Store

--- client-frontend/src/pages/admin/admin.jsx
+++ client-frontend/src/pages/admin/admin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import "./AdminPanel.css";
+import "./AdminPanel.css";
 
 function AdminPanel() {
   const [Products, setProducts] = useState([]);
