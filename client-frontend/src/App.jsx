e99b588f29ebc64fa3ea32fc040d0eabe53c9c63
IT21158704 <it21158704@my.sliit.lk>
2023-09-09 11:28:42 +0530

    Store Update

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -1,18 +1,30 @@
 import Canvas from "./canvas"
+import React from "react";
+import Navbar from "./components/navbar/Navbar"
+import { CartProvider } from "./context/Context"
 import Customizer from "./pages/Customizer"
 import Home from "./pages/Home"
+import AdminPanel from "./pages/admin/admin"
+import Cart from "./pages/cart/Cart"
+import Homepage from "./pages/homepage/Homepage"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <main className="app transition-all ease-in">
-        <h4 className="head-text">Virtual Dressing Room</h4>
-        <Home/>
-        <Canvas/>
-        <Customizer/>
-      </main>
-    </>
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <div className="App">
+          <Routes>
+          <Route path="/" element={<Homepage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/admin" element={<AdminPanel />} />
+            
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   )
 }
 
