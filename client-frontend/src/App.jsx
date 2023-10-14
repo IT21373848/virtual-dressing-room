6eaebdadb2b639220715771807f6eed1dc947539
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2023-10-14 17:26:12 +0530

    Dress_Room
    Dress_Room

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -18,6 +18,11 @@ import LandingPage from "./pages/landingpage";
 import NavbarLogin from "./components/navbar/Navbar-login";
 import CreateBlog from "./pages/admin/CreateBlog";
 import AllBlogs from "./pages/admin/AllBlogs";
+
+import MyHome from './pages/DressingRoom/MyHome'
+import AppMale from './pages/DressingRoom/AppMale'
+import AppFemale from './pages/DressingRoom/AppFemale'
+
 function App() {
   return (
     <CartProvider>
@@ -37,6 +42,9 @@ function App() {
             <Route path="/blog" element={<Blog />} />
             <Route path="/admin/blog" element={<CreateBlog />} />
             <Route path="/admin/allblog" element={<AllBlogs />} />
+            <Route path="/dressroom" element = {<MyHome/>}/>
+            <Route path="/dressroom/male" element = {<AppMale/>}/>
+            <Route path="/dressroom/Female" element = {<AppFemale/>}/>
 
           </Routes>
         </div>
